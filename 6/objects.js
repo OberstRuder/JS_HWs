a5ec208e886dc86c195a6fc210f12cbb9a848e79
Бабко Евгений <doxis32123@gmail.com>
2023-10-31 11:31:45 +0200

    hw6_start

---
+++ 6/objects.js
@@ -0,0 +1,29 @@
+{
+    //Literals
+    let cat = {
+        color: "gray",
+        weight: "5kg",
+        years: "3"
+   }
+
+    let car = {
+        color: "black",
+        weight: "1600kg",
+        dateOfManufacture: "14.07.2019",
+        clearance: "20cm",
+        engine: "1.2 BMD"
+    }
+
+    let laptop = {
+        color: "black",
+        weight: "2kg",
+        dimensions: "362.3 x 237.4 x 19.9 mm",
+        ScreenDiagonal: "15.6",
+        Processor: "Intel Core i5-12450H"
+    }
+}
+
+{
+    //Literals expand
+    
+}
